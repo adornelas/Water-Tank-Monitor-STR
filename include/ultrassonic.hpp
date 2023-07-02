f5232af528dc9f41f4cc32f51bd3583e5ab530d6
André <andredornelas23@gmail.com>
2023-07-02 16:01:12 -0300

    Implementation of ultrassonic header

---
+++ include/ultrassonic.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Arduino.h>
+
+// ESP32 GPIOs
+#define trig 12
+#define echo 13
+
+
+namespace Ultrassonic{
+
+    int MaxLevel = 13;
+
+    void setup();
+    /// @brief Measure water level based on MaxLevel.
+    /// @return Water height in cm.
+    int measureWaterLevel();
+}
