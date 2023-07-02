9545862dc2fc94f123ffca3a005f14af462cb2fc
Hiago dos Santos <hiagop22@gmail.com>
2023-07-02 11:37:32 -0300

    Using FreeRtos

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
 
+int count1 = 0;
+int count2 = 0;
+
+void task1(void * parameters){
+  for(;;){
+    Serial.print("Task 1 counter: ");
+    Serial.println(count1++);
+    
+    vTaskDelay(500/portTICK_PERIOD_MS);
+  }
+}
+
+void task2(void * parameters){
+  for(;;){
+    Serial.print("Task 2 counter: ");
+    Serial.println(count2++);
+    
+    vTaskDelay(500/portTICK_PERIOD_MS);
+  }
+}
+
 void setup() {
   Serial.begin(9600);
+
+  xTaskCreate(
+    task1,
+    "Task 1",
+    1000,
+    NULL,
+    1,
+    NULL
+  );
+
+  xTaskCreate(
+    task2,
+    "Task 1",
+    1000,
+    NULL,
+    2,
+    NULL
+  );
+
 }
 
 void loop() {
-  Serial.println("Teste.");
+  
 }
