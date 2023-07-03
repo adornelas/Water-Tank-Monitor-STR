957a613b2f607c8f02b3cf406c36055258f410f4
André <andredornelas23@gmail.com>
2023-07-02 21:02:07 -0300

    new functions signatures

--- include/ultrassonic.hpp
+++ include/ultrassonic.hpp
@@ -9,10 +9,16 @@
 
 namespace Ultrassonic{
 
+    extern int WaterLevel;
+
     const int MaxLevel = 13;
 
     void setup();
+    void Task_Measure(void *parameters);
+    /// @brief Set the Global Variable Water Level
+    /// @param water_level 
+    void SetWaterLevel(int water_level);
     /// @brief Measure water level based on MaxLevel.
-    /// @return Water height in cm.
-    int measureWaterLevel();
+    /// @param water_level pointer to water level variable.
+    void measureWaterLevel(int *water_level);
 }
