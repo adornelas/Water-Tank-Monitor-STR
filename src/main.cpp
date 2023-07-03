f6737e80b8b95a833d780119c03974b01a42bcb6
André <andredornelas23@gmail.com>
2023-07-02 21:43:49 -0300

    cria Mutex de variavel global

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 
 #include "ultrassonic.hpp"
 
+SemaphoreHandle_t xMutex_Var = NULL;
 int WaterLevel;
 
 void setup() {
   Serial.begin(115200);
   Serial.printf("\n\rSistema de Monitoramento de Reservatório\n\r");
+  
+  xMutex_Var = xSemaphoreCreateMutex();
+  if(xMutex_Var == NULL)
+  {
+    Serial.printf("\n\rFalha em criar o Mutex para variavel global");
+  }
 }
 
 void loop() {
