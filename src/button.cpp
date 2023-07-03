2d37faca2f2831fe0791db2756dd6f4afd22410b
Hiago dos Santos <hiagop22@gmail.com>
2023-07-02 22:49:05 -0300

    Getting button infos

--- src/button.cpp
+++ src/button.cpp
@@ -8,56 +8,79 @@
 static SemaphoreHandle_t xSemaphore_ManualButton = NULL;
 static SemaphoreHandle_t xSemaphore_AutomaticButton = NULL;
 static SemaphoreHandle_t xSemaphore_StopButton = NULL;
+static SemaphoreHandle_t xMutex_SystemState = NULL;
 
 namespace Button{
-    static int ManualButton;
-    static int AutomaticButton;
-    static int StopButton;
+    static int pinManualButton;
+    static int pinAutomaticButton;
+    static int pinStopButton;
 
+    static unsigned char SystemState;
+
+    void setup(void);
     void SetManualButtom(int value);
     void SetAutomaticButtom(int value);
     void SetStopButtom(int value);
+
+    unsigned char GetButtonState(void);
     
-    void IRAM_ATTR PushManualButton(void);
-    void IRAM_ATTR PushAutomaticButton(void);
-    void IRAM_ATTR PushStopButton(void);
+    static void IRAM_ATTR PushManualButton(void);
+    static void IRAM_ATTR PushAutomaticButton(void);
+    static void IRAM_ATTR PushStopButton(void);
 
 }
 
-void Button::SetManualButtom(int value){
-    ManualButton=value;
+void Button::setup(void){
+    xMutex_SystemState = xSemaphoreCreateMutex();
+    if(xMutex_SystemState == NULL){
+        Serial.printf("\n\rFalha em criar o Mutex para o botão desejado");
+    }
+}
 
-    pinMode(ManualButton, INPUT);
-    attachInterrupt(digitalPinToInterrupt(ManualButton), 
+void Button::SetManualButtom(int value){
+    pinManualButton=value;
+
+    pinMode(pinManualButton, INPUT);
+    attachInterrupt(digitalPinToInterrupt(pinManualButton), 
                     Button::PushManualButton, 
                     RISING);
 
     vSemaphoreCreateBinary(xSemaphore_ManualButton);
+
+    if(xSemaphore_ManualButton == NULL){
+        Serial.printf("\n\rFalha em criar o semafaro para botão manual");
+    }
 }
 
 void Button::SetAutomaticButtom(int value){
-    AutomaticButton=value;
+    pinAutomaticButton=value;
 
-    pinMode(AutomaticButton, INPUT);
-    attachInterrupt(digitalPinToInterrupt(AutomaticButton), 
+    pinMode(pinAutomaticButton, INPUT);
+    attachInterrupt(digitalPinToInterrupt(pinAutomaticButton), 
                     Button::PushAutomaticButton, 
                     RISING);
 
     vSemaphoreCreateBinary(xSemaphore_AutomaticButton);
+    if(xSemaphore_ManualButton == NULL){
+        Serial.printf("\n\rFalha em criar o semafaro para botão automático");
+    }
 }
 
 void Button::SetStopButtom(int value){
-    StopButton=value;
+    pinStopButton=value;
 
-    pinMode(StopButton, INPUT);
-    attachInterrupt(digitalPinToInterrupt(StopButton), 
+    pinMode(pinStopButton, INPUT);
+    attachInterrupt(digitalPinToInterrupt(pinStopButton), 
                     Button::PushStopButton, 
                     RISING);
 
     vSemaphoreCreateBinary(xSemaphore_StopButton);
+    if(xSemaphore_ManualButton == NULL){
+        Serial.printf("\n\rFalha em criar o semafaro para botão stop");
+    }
 }
 
-void IRAM_ATTR PushAutomaticButton(void){
+static void IRAM_ATTR PushManualButton(void){
     static uint32_t last_time = 0;
 
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
@@ -67,6 +90,63 @@ void IRAM_ATTR PushAutomaticButton(void){
         last_time = millis();
         xSemaphoreGiveFromISR(xSemaphore_ManualButton, NULL);
     }
+}
+
+static void IRAM_ATTR PushAutomaticButton(void){
+    static uint32_t last_time = 0;
+
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    // Algoritmo de debounce do botão
+    if( (millis() - last_time) >= DEBOUNCE_BUTTON){
+        last_time = millis();
+        xSemaphoreGiveFromISR(xSemaphore_AutomaticButton, NULL);
+    }
+}
+
+static void IRAM_ATTR PushStopButton(void){
+    static uint32_t last_time = 0;
+
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    // Algoritmo de debounce do botão
+    if( (millis() - last_time) >= DEBOUNCE_BUTTON){
+        last_time = millis();
+        xSemaphoreGiveFromISR(xSemaphore_StopButton, NULL);
+    }
+}
+
+unsigned char Button::GetButtonState(void){
+    unsigned char retState;
+
+    if(xSemaphoreTake(xSemaphore_ManualButton,portMAX_DELAY) == pdTRUE){
+        xSemaphoreTake(xMutex_SystemState,portMAX_DELAY);
+
+        SystemState = 'M';
+        retState = SystemState;
+
+        xSemaphoreGive(xMutex_SystemState);
+    }
+
+    if(xSemaphoreTake(xSemaphore_AutomaticButton,portMAX_DELAY) == pdTRUE){
+        xSemaphoreTake(xMutex_SystemState,portMAX_DELAY);
+
+        SystemState = 'A';
+        retState = SystemState;
+
+        xSemaphoreGive(xMutex_SystemState);
+    }
+
+    if(xSemaphoreTake(xSemaphore_StopButton,portMAX_DELAY) == pdTRUE){
+        xSemaphoreTake(xMutex_SystemState,portMAX_DELAY);
+
+        SystemState = 'S';
+        retState = SystemState;
+
+        xSemaphoreGive(xMutex_SystemState);
+    }
+
+    return retState;
 }
 
 
