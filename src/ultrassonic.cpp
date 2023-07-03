c6c9a98ecf607fb75fd031ff988742d3653b9cb3
André <andredornelas23@gmail.com>
2023-07-02 21:03:08 -0300

    Implements Task measure water level

--- src/ultrassonic.cpp
+++ src/ultrassonic.cpp
@@ -7,10 +7,26 @@ namespace Ultrassonic {
         pinMode(echo, INPUT);
         long t = pulseIn(echo, HIGH);
         int distance = t / 29 / 2;
-        
+
+        // Cria tarefa do sensor
+        xTaskCreate(Task_Measure, "Measure", configMINIMAL_STACK_SIZE * 2, NULL, tskIDLE_PRIORITY + 1, NULL);   
     }
 
-    int measureWaterLevel() {
+    void Task_Measure(void *parameters){
+        int water_level;
+        // TODO: desabilitar interrupções para calculo correto do nível
+        measureWaterLevel(&water_level);
+        SetWaterLevel(water_level);
+    }
+
+    void SetWaterLevel(int water_level)
+    {
+        // Obtem o Mutex Variavel Global
+        WaterLevel = water_level;
+        // libera o Mutex Variavel Global
+    }
+
+    void measureWaterLevel(int *water_level) {
         digitalWrite(trig, LOW);
         delayMicroseconds(4);
         digitalWrite(trig, HIGH);
@@ -19,7 +35,7 @@ namespace Ultrassonic {
         long t = pulseIn(echo, HIGH);
         int distance = t / 29 / 2;
 
-        return (MaxLevel - distance);
+        *water_level = (MaxLevel - distance);
     }
 
 }
