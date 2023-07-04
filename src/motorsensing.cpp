394f5485a5163b47a25c9689faeb00115d547c62
André <andredornelas23@gmail.com>
2023-07-03 21:03:19 -0300

    Task is now thread, created in main

--- src/motorsensing.cpp
+++ src/motorsensing.cpp
@@ -2,7 +2,6 @@
 
 namespace MotorSensing {
 
-    SemaphoreHandle_t xMutex_Var_MotorInfo;
     motorInfoStruct motorInfo;
 
     ACS712 sensor(ACS712_05B, CURRENT_GPIO);
@@ -13,16 +12,23 @@ namespace MotorSensing {
         Serial.println("Calibrating electric current sensor... Ensure that no current flows through the sensor at this moment");
         sensor.calibrate();
         dht.begin();
-
-        xTaskCreate(Task_MeasureMotor, "Measure_Current", configMINIMAL_STACK_SIZE * 2, NULL, tskIDLE_PRIORITY + 1, NULL);   
     }
 
     void Task_MeasureMotor(void *parameters){
         int current_value;
         motorInfoStruct motor_info;
-        // TODO: desabilitar interrupções para calculo correto dos valores
-        measureMotor(&motor_info);
-        SetMotorInfoValue(motor_info.current, motor_info.temperature);
+        while (1)
+        {
+            // TODO: desabilitar interrupções para calculo correto dos valores
+            Serial.println(F("T:Medindo a temperatura"));
+            measureMotor(&motor_info);
+            Serial.print(F("T:mediu "));
+            Serial.println(motor_info.temperature);
+            SetMotorInfoValue(motor_info.current, motor_info.temperature);
+            Serial.println(F("T:setou, Delay"));
+            Serial.println(F("T:Final da task measure tempertture"));
+            vTaskDelay(1000/portTICK_PERIOD_MS);
+        }
     }
 
     void SetMotorInfoValue(float current_value, float temperatura_value)
@@ -31,14 +37,17 @@ namespace MotorSensing {
         xSemaphoreTake(xMutex_Var_MotorInfo,portMAX_DELAY );
         motorInfo.current = current_value;
         motorInfo.temperature = temperatura_value;
+        Serial.println(F("T:atribuiu"));
         // Releases the Global Variable Mutex
         xSemaphoreGive(xMutex_Var_MotorInfo);
     }
 
     void measureMotor(motorInfoStruct *motor_info) {
-        float I = sensor.getCurrentDC();
+        float I;
+        float t;
+        I = sensor.getCurrentDC();
         // TODO: garantir que as leituras sejam feitas com ao menos 2s de espaçamento
-        float t = dht.readTemperature();
+        t = dht.readTemperature();
         if (isnan(t)) {
             Serial.println(F("Failed to read from DHT sensor!"));
         } else {
--- src/ultrassonic.cpp
+++ src/ultrassonic.cpp
@@ -1,25 +1,31 @@
 #include "ultrassonic.hpp"
 
 namespace Ultrassonic {
-
-    SemaphoreHandle_t xMutex_Var_Water;
+    
     int WaterLevel;
 
     void setup() {
-        pinMode(trig, OUTPUT);
-        pinMode(echo, INPUT);
-        long t = pulseIn(echo, HIGH);
+        pinMode(TRIG, OUTPUT);
+        pinMode(ECHO, INPUT);
+        long t = pulseIn(ECHO, HIGH);
         int distance = t / 29 / 2;
 
-        // Cria tarefa do sensor
-        xTaskCreate(Task_Measure_Water, "Measure_Water", configMINIMAL_STACK_SIZE * 2, NULL, tskIDLE_PRIORITY + 1, NULL);   
     }
 
     void Task_Measure_Water(void *parameters){
         int water_level;
+        while (1)
+        {        
         // TODO: desabilitar interrupções para calculo correto do nível
+        Serial.println(F("W:Medindo a agua"));
         measureWaterLevel(&water_level);
+        Serial.print(F("W:mediu "));
+        Serial.println(water_level);
         SetWaterLevel(water_level);
+        Serial.println(F("W:Delay"));
+        Serial.println(F("W:Final da task measure water"));
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        }
     }
 
     void SetWaterLevel(int water_level)
@@ -27,17 +33,20 @@ namespace Ultrassonic {
         // Obtains the Global Variable Mutex
         xSemaphoreTake(xMutex_Var_Water,portMAX_DELAY );
         WaterLevel = water_level;
+        Serial.println(F("W:atribuiu"));
         // Releases the Global Variable Mutex
         xSemaphoreGive(xMutex_Var_Water);
     }
 
     void measureWaterLevel(int *water_level) {
-        digitalWrite(trig, LOW);
-        delayMicroseconds(4);
-        digitalWrite(trig, HIGH);
-        delayMicroseconds(10);
-        digitalWrite(trig, LOW);
-        long t = pulseIn(echo, HIGH);
+        digitalWrite(TRIG, LOW);
+        vTaskDelay(4/portTICK_PERIOD_MS);
+        // delayMicroseconds(4);
+        digitalWrite(TRIG, HIGH);
+        // delayMicroseconds(10);
+        vTaskDelay(10/portTICK_PERIOD_MS);
+        digitalWrite(TRIG, LOW);
+        long t = pulseIn(ECHO, HIGH);
         int distance = t / 29 / 2;
 
         *water_level = (MaxLevel - distance);
