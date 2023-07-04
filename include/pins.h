e9eb37700b2716be4d36e3c2066fc8e63b7cf651
André <andredornelas23@gmail.com>
2023-07-03 21:02:09 -0300

    adds pins header

---
+++ include/pins.h
@@ -0,0 +1,17 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define RELAY_PIN 10
+
+#define MANUAL_BUTTON 35
+#define STOP_BUTTON 4
+#define AUTOMATIC_BUTTON 32
+
+
+#define CURRENT_GPIO 36
+#define TEMPERATURE_GPIO 21
+
+#define TRIG 14
+#define ECHO 12
+
+#endif
