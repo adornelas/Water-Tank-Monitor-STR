71f1525da87144dac8ec803ff54fad803b84297d
adornelas <andredornelas23@gmail.com>
2023-07-05 17:04:30 -0300

    Implementa LCD

--- include/lcd.hpp
+++ include/lcd.hpp
@@ -5,15 +5,17 @@
 #include <LiquidCrystal_I2C.h>
 #include "pins.h"
 #include "RTS_settings.h"
-
-extern SemaphoreHandle_t xMutex_Var_Water;
+#include "ultrassonic.hpp"
+#include "motorsensing.hpp"
+#include "buttons.hpp"
 
 namespace LCD{
 
     extern int WaterLevel;
-
-    const int MaxLevel = 15;
+    extern srcSystem SystemState;
 
     void setup();
     void Task_LCD(void *parameters);
+    void writeState(srcSystem system_state);
+    void writeWaterLevel(int water_level);
 }
