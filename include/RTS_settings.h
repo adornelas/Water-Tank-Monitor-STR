2dc28e60b0d03ccca0bff7081782e16e0e0fa163
André <andredornelas23@gmail.com>
2023-07-05 10:11:53 -0300

    fixes priorities and clean comments

--- include/RTS_settings.h
+++ include/RTS_settings.h
@@ -1,24 +1,21 @@
 #ifndef RTOS_SETTINGS_H
 #define RTOS_SETTINGS_H
 
-// #define configGENERATE_RUN_TIME_STATS 1
+#define PRINT_DEBUG true
 
-// char *stats;
+#define HANDLE_STOP_PRIORITY        1
+#define PUMP_PRIORITY               2
+#define MEASURE_MOTOR_PRIORITY      3
+#define MEASURE_WATER_PRIORITY      4
+#define HANDLE_AUTOMATIC_PRIORITY   5
+#define HANDLE_MANUAL_PRIORITY      6
+#define LCD_PRIORITY                7
+#define UPLOAD_STATUS_PRIORITY      8
 
-#define PRINT_DEBUG false
-#define STATS_DEBUG false
-
-#define MEASURE_WATER_PRIORITY      3
-#define MEASURE_MOTOR_PRIORITY      2
-#define PUMP_PRIORITY               1
-#define HANDLE_AUTOMATIC_PRIORITY   4
-#define HANDLE_MANUAL_PRIORITY      5
-#define HANDLE_STOP_PRIORITY        6
-#define UPLOAD_STATUS_PRIORITY      7
-
-#define MEASURE_WATER_PERIOD 500
-#define MEASURE_MOTOR_PERIOD 2000
-#define PUMP_PERIOD 500
-#define UPLOAD_STATUS_PERIOD 2000
+#define MEASURE_WATER_PERIOD    500
+#define MEASURE_MOTOR_PERIOD    2000
+#define PUMP_PERIOD             500
+#define UPLOAD_STATUS_PERIOD    2
+#define LCD_PERIOD              1000
 
 #endif
